6bdc186416f0b506bb0d008e8a68e36752562901
Micaella Morales <morales.micaella@gmail.com>
2020-03-09 15:15:29 -0700

    added comments

--- assignment2.c
+++ assignment2.c
@@ -6,9 +6,11 @@
 * File: assignment2.c
 *
 * Description:
-*
+* This file uses the WiringPi library to create a tapeless
+* ruler. An echo sensor (HC-SR04) was used to measure
+* the distance.
 * To compile this file, enter make.
-* To run this file, enter 
+* To run this file, enter ./assignment2
 ****************************************************************/
 
 #include <stdio.h>
@@ -19,12 +21,14 @@
 #define echoPin 22
 #define SPEED_OF_SOUND 340
 
+// initializes the wiringPi and sets up the trigger and echo pins
 void setup() {
     wiringPiSetup();
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
+// calculates the distance using the formula distance = (velocity*time)/2
 double calculate_distance() {
     double distance;
     clock_t start, end, time_elapsed;
@@ -37,15 +41,18 @@ double calculate_distance() {
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 
+    // records the time when ultrasonic is transmitted
     while(digitalRead(echoPin) == 0)
         start = clock();
     
+    // records the time when ultrasonic is received.
     while(digitalRead(echoPin) == 1) 
         end = clock();
     
     time_elapsed = (double)(end-start);
     distance = (time_elapsed * SPEED_OF_SOUND) / 2.0;
     
+    // returns distance in the unit of cm
     return distance * 100 / CLOCKS_PER_SEC;
 }
 
@@ -55,7 +62,7 @@ int main(void) {
     
     while(1) {
         distance = calculate_distance();
-        printf("Distance: %f cm \n", distance);
+        printf("Distance: %fcm \n", distance);
     } 
     
     return 0;
